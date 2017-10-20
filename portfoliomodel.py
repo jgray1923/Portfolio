03874e91b950680c55a2aea23f262e7ec6cfa7b7
jgray1923 <32068677+jgray1923@users.noreply.github.com>
2017-10-20 22:08:40 +0100

    Code Shrinking I
    Completion of the second module practical

--- portfoliomodel.py
+++ portfoliomodel.py
@@ -6,77 +6,105 @@ Created on Fri Oct 20 14:47:14 2017
 """
 # Import modules required for the ABM
 import random
+import operator
+import matplotlib.pyplot
 
-# Create variables for agent's location
-# Create an x coordinate
-x0 = 50
-# Create a y coordinate
-y0 = 50
+# Create a list for agents
+agents =[]
 
-print("Agent 0's starting coordinates are", " ", x0, y0)
+# Use the agents list to create random starting locations of agent using random integers
+# in order to remove x and y variables for cleaner code
+agents.append([random.randint(0,100), random.randint(0,100)])
+
+# The first dimenstion within the list refers to each agent
+# The second dimension within the list; 0 is the x coord, 1 is the y coord
+
+# Print the starting coordinates of the first(and currently the only) agent
+print("Agent 0's starting coordinates are", " ", agents[0])
 
 # Step 1 of agent 0's movement
-# Change x coordinate based on random numbers
+# Change x coordinate based on random numbers using if else statement
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][0] += 1
 else:
-    x0 -= 1
+    agents[0][0] -= 1
 
-# Change y coordinate based on random numbers
+# Change y coordinate based on random numbers using if else statement
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][1] += 1
 else:
-    y0 -= 1
+    agents[0][1] -= 1
 
 # Print to check code for agent's movement is working
-print("Agent 0's location after step 1 is", " ", x0,y0)    
+print("Agent 0's location after step 1 is", " ", agents[0])    
 
-# Step 2 of agent's movement
+# Step 2 of agent 0's movement
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][0] += 1
 else:
-    x0 -= 1
+    agents[0][0] -= 1
     
 if random.random() < 0.5:
-    y0 += 1
-else:
-    y0 -= 1    
+    agents[0][1] += 1
+# else:
+    agents[0][1] -= 1    
     
 # Print to check code for agent's movement is working
-print("Agent 0's location after step 2 is", " ", x0, y0)
+print("Agent 0's location after step 2 is", " ", agents[0])
 
-# Create a second random agent with random movement over two steps
-x1 = random.randint(0,100)
-y1 = random.randint(0,100)
 
-print("Agent 1's starting coordinates are", " ", x1, y1)
+# Create a second agent by appending random integers to agents list
+agents.append([random.randint(0,100), random.randint(0,100)])
+
+# Print the starting coordinates of the second agent, agent 1
+print("Agent 1's starting coordinates are", " ", agents[1])
+
+# Move the second agent randomly using if else statement
+if random.random() < 0.5:
+    agents[1][0] += 1
+else:
+    agents[1][0] -= 1
 
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
-
-if random.random() < 0.5:
-    y1 += 1
-else:
-    y1 -= 1
+    agents[1][1] -= 1
     
-print("Agent 1's location after step 1 is", " ", x1,y1)
+print("Agent 1's location after step 1 is", " ", agents[1])
 
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][0] += 1
 else:
-    x1 -= 1
+    agents[1][0] -= 1
 
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][1] += 1
 else:
-    y1 -= 1
+    agents[1][1] -= 1
     
-print("Agent 1's location after step 2 is", " ", x1,y1)
-    
+print("Agent 1's location after step 2 is", " ", agents[1])
 
 # Work out the pythagorous distance between the two sets of x's and y's
-answer = ((x0 - x1)**2) + ((y0 - y1)**2)** 0.5
+answer = ((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2)** 0.5
 print("The distance between the two agents is", " ", answer)
+
+
+# Conduct some analysis on the agent's coordinates 
+print(max(agents))
+# This will only take into consideration the y coordinate if all of the x
+# coordinates are the same. So this will thereforeprint the set of coords 
+# with the maximum number in it
+
+# However, the .itemgettier variable from the operator package will take into
+# account the y variable due to grabbbing it [1] from the agents list
+# so this will give the coords with both the maximum x [0] and y[1] number
+
+print(max(agents, key=operator.itemgetter(1)))
+
+# Use matplotlib.pyplot to plot the current locations of agent 0 and agent 1
+matplotlib.pyplot.ylim(0, 100)
+matplotlib.pyplot.xlim(0, 100)
+matplotlib.pyplot.scatter(agents[0][1],agents[0][0], color='pink')
+matplotlib.pyplot.scatter(agents[1][1],agents[1][0], color='purple')
+matplotlib.pyplot.show()
 
