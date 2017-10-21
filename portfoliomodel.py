823fccea0cf16c41fc086968462528c548288f44
jgray1923 <32068677+jgray1923@users.noreply.github.com>
2017-10-21 12:44:11 +0100

    Building Tools
    Completion of the fourth module practical, building tools

--- portfoliomodel.py
+++ portfoliomodel.py
@@ -13,6 +13,10 @@ num_of_agents = 10
 # Create variable for number of agent's movements in foor loop
 num_of_iterations = 100
 
+# Define function to calculate distance between two agents' pairs of coordinates
+def distance_between(agent0, agent1):
+    return (((agent0[0] - agent1[0])**2) + ((agent0[1] - agent1[1])**2))**0.5
+
 # Create a list for agents
 agents =[]
 
@@ -42,8 +46,8 @@ print("Current agents' locations are", " ", agents)
 
 
 # Work out the pythagorous distance between the two sets of x's and y's
-# answer = ((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2)** 0.5
-# print("The distance between the two agents is", " ", answer)
+#answer = ((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2)** 0.5
+#print("The distance between the two agents is", " ", answer)
 
 
 
@@ -53,4 +57,12 @@ matplotlib.pyplot.xlim(0, 100)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i][0],agents[i][1], color='pink')
 matplotlib.pyplot.show()
+
+# Create for loop to work out the distances between the agent's locations
+# agent0 refers to each seperate agent, and agent1 refers to each agent's coords
+# so the distance given will be between agent0 and agent1, agent1 and agent2 etc.
+for agent0 in agents:
+    for agent1 in agents:
+        distance = distance_between(agent0, agent1)
+    print("Distance between agents is", " ", distance)
 
