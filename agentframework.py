d12ccc26250ed26d9e142828df621265f846b179
jgray1923 <32068677+jgray1923@users.noreply.github.com>
2017-10-21 20:22:21 +0100

    I/O
    Completion of sixth module practical, I/O

--- agentframework.py
+++ agentframework.py
@@ -15,21 +15,38 @@ class Agent():
 # instance of the class, eg the agent's x and y coordinates
 
 # Define 'self'
-    def __init__(self):
-# Create random integers for x and y coordinates of agents within the extent 
-# of the environment (100x100 grid)
-        self.x = random.randint(0,100)
-        self.y = random.randint(0,100)
+    def __init__(self, environment):
+# Set the environment as environment, the file opened in portfoliomodel.py
+        self.environment = environment
+# Create random integers for x and y coordinates of agents within the full extent 
+# of the environment (300x300 grid)
+        self.x = random.randint(0,300)
+        self.y = random.randint(0,300)
+# Set the store (the sheep's(agents') stomach), as zero (empty stomach)
+        self.store = 0
     
-# Define 'move' for the random movement of agents within the 100x100 neighbourhood        
+# Define 'move' for the random movement of agents within the 300x300 neighbourhood        
     def move(self):
         if random.random() < 0.5:
-            self.x = (self.x + 1) % 100
+            self.x = (self.x + 1) % 300
         else:
-            self.x = (self.x - 1) % 100
+            self.x = (self.x - 1) % 300
     
         if random.random() < 0.5:
-            self.y = (self.y + 1) % 100
+            self.y = (self.y + 1) % 300
         else:
-            self.y = (self.y - 1) % 100
+            self.y = (self.y - 1) % 300
+# Define 'eat' for the sheep to graze on the environment            
+    def eat(self):
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 30
+        else:
+            self.store = self.store
+# Create if statement to make the sheep sick (empty their stomach(store)), if they
+# have eaten more than 100 of the environment        
+        if self.store <= 100:
+            self.store = 0
+
+        
             
--- portfoliomodel.py
+++ portfoliomodel.py
@@ -7,14 +7,37 @@ Created on Fri Oct 20 14:47:14 2017
 # Import modules required for the ABM
 import random
 import matplotlib.pyplot
+import csv
 
 # Import code required for the ABM
 import agentframework
 
+# Create list that will hold the environment data
+environment = []
+
+# Open CSV file for modelling the environment
+f = open('in.txt', newline='')
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+
+for row in reader:
+# A new rowlist generated each time the loop runs.
+    rowlist = []		
+    
+    for value in row:	
+        rowlist.append(value)
+             
+        #print(value) 	
+    environment.append(rowlist)
+f.close()
+
+# Generate a graph to display the environment data
+#matplotlib.pyplot.imshow(environment)
+#matplotlib.pyplot.show()
+
 # Create variable for number of agents
-num_of_agents = 10
+num_of_agents = 20
 # Create variable for number of agent's movements in foor loop
-num_of_iterations = 100
+num_of_iterations = 150
 
 # Define function to calculate distance between two agents' pairs of coordinates
 def distance_between(agent0, agent1):
@@ -23,19 +46,22 @@ def distance_between(agent0, agent1):
 # Create a list for agents
 agents =[]
 
-# Create for loop for creating random starting coordinates for agents
+# Create for loop for creating random starting coordinates for agents, within 
+# the given environment
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent())
+    agents.append(agentframework.Agent(environment))
 
 # Due to the for loop, we can now refer to each agent as agents[i]
 # Due to the Agent class, we can refer to the x coordinate as agents[i].x
 # and we can refer to the y coordinate as agents[i].y
 
-# Create for loop for the movement of agent's one by one, with num_of_iterations variable
-# and 'move' from agentframework
+
+# Create for loop for the movement of agents (sheep), and their subsequent 
+# eating/grazing throughout the environment
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
         agents[i].move()
+        agents[i].eat()
         
 
 # Work out the pythagorous distance between the two sets of x's and y's
@@ -43,11 +69,20 @@ for j in range(num_of_iterations):
 #print("The distance between the two agents is", " ", answer)
 
 
-# Use matplotlib.pyplot to plot the current locations of agents
-matplotlib.pyplot.ylim(0, 100)
-matplotlib.pyplot.xlim(0, 100)
+## Use matplotlib.pyplot to plot the current locations of agents
+#matplotlib.pyplot.ylim(0, 100)
+#matplotlib.pyplot.xlim(0, 100)
+#for i in range(num_of_agents):
+#    matplotlib.pyplot.scatter(agents[i].x, agents[i].y, color='pink')
+#matplotlib.pyplot.show()
+
+# Create a graph to show the current location of the sheep within the environment
+# displaying where the sheep have been grazing 
+matplotlib.pyplot.xlim(0, 300)
+matplotlib.pyplot.ylim(0, 300)
+matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i].x, agents[i].y, color='pink')
+    matplotlib.pyplot.scatter(agents[i].x,agents[i].y, color='gray')
 matplotlib.pyplot.show()
 
 # Create for loop to work out the distances between the agent's locations
@@ -57,3 +92,10 @@ for agent0 in agents:
     for agent1 in agents:
         distance = distance_between(agent0, agent1)
     
+# Write out the environment as a file
+f2 = open('dataout.csv', 'w', newline='') 
+writer = csv.writer(f2, delimiter=',')
+for row in environment:		
+	writer.writerow(row)		
+f2.close()
+
